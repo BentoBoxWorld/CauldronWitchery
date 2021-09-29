c8a1bdc7094c5e7bb7efffc5bb7dee5935bc5640
BONNe <bonne@bonne.id.lv>
2021-09-29 12:37:20 +0300

    Add new features to the MagicStickObject:
    - Name: ability to change name of magic stick.
    - Description: ability to set custom lore for item.
    - Permissions: ability to set permissions for stick.
    - Complexity: ability to set complexity of stick usage.

--- src/main/java/world/bentobox/cauldronwitchery/database/object/MagicStickObject.java
+++ src/main/java/world/bentobox/cauldronwitchery/database/object/MagicStickObject.java
@@ -10,12 +10,13 @@ package world.bentobox.cauldronwitchery.database.object;
 import com.google.gson.annotations.Expose;
 import org.bukkit.inventory.ItemStack;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import world.bentobox.bentobox.database.objects.DataObject;
 import world.bentobox.bentobox.database.objects.Table;
-import world.bentobox.cauldronwitchery.database.object.recipe.BookRecipe;
 import world.bentobox.cauldronwitchery.database.object.recipe.Recipe;
 
 
@@ -127,7 +128,101 @@ public class MagicStickObject implements DataObject
             map(Recipe::clone).
             collect(Collectors.toList()));
 
+        object.setFriendlyName(this.friendlyName);
+        object.setDescription(this.description);
+
+        object.setPermissions(new HashSet<>(this.getPermissions()));
+        object.setComplexity(this.complexity);
+
         return object;
+    }
+
+
+    /**
+     * Gets friendly name.
+     *
+     * @return the friendly name
+     */
+    public String getFriendlyName()
+    {
+        return friendlyName;
+    }
+
+
+    /**
+     * Sets friendly name.
+     *
+     * @param friendlyName the friendly name
+     */
+    public void setFriendlyName(String friendlyName)
+    {
+        this.friendlyName = friendlyName;
+    }
+
+
+    /**
+     * Gets description.
+     *
+     * @return the description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+
+    /**
+     * Sets description.
+     *
+     * @param description the description
+     */
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+
+    /**
+     * Gets permissions.
+     *
+     * @return the permissions
+     */
+    public Set<String> getPermissions()
+    {
+        return permissions;
+    }
+
+
+    /**
+     * Sets permissions.
+     *
+     * @param permissions the permissions
+     */
+    public void setPermissions(Set<String> permissions)
+    {
+        this.permissions = permissions;
+    }
+
+
+    /**
+     * Gets complexity.
+     *
+     * @return the complexity
+     */
+    public float getComplexity()
+    {
+        return complexity;
+    }
+
+
+    /**
+     * Sets complexity.
+     *
+     * @param complexity the complexity
+     */
+    public void setComplexity(float complexity)
+    {
+        this.complexity = complexity;
     }
 
 
@@ -149,6 +244,27 @@ public class MagicStickObject implements DataObject
     private ItemStack magicStick;
 
     /**
+     * The name of magic stick.
+     * Mostly for GUI's.
+     */
+    @Expose
+    private String friendlyName;
+
+    /**
+     * The description of magic stick.
+     * Mostly for GUI's.
+     */
+    @Expose
+    private String description;
+
+    /**
+     * Permissions for using the stick.
+     * Stricter over recipe permissions.
+     */
+    @Expose
+    private Set<String> permissions = new HashSet<>();
+
+    /**
      * List of recipes for this magic stick.
      */
     @Expose
@@ -159,4 +275,12 @@ public class MagicStickObject implements DataObject
      */
     @Expose
     private String bookName;
+
+    /**
+     * Allows to set complexity of the stick usage.
+     * Larger complexity, bigger chance for it to fail.
+     * TODO: Requires for MASTERY_SKILL feature
+     */
+    @Expose
+    private float complexity;
 }
